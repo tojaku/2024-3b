8261867fe4d0692a7802bfa40d82e359439ce136
Tomislav <tojaku@gmail.com>
2025-03-27 09:40:05 +0100

    upd 25.03.27

---
+++ niop/GUI_Test/src/main/java/hr/obskc/niop/gui_test/PanelActionListener.java
@@ -0,0 +1,9 @@
+package hr.obskc.niop.gui_test;
+
+import java.util.HashMap;
+
+public interface PanelActionListener {
+    // 1. korak - definicija izgleda promatrača
+
+    void onPanelAction(HashMap<String, String> data);
+}
